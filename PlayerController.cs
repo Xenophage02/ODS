8a09eface42c3b488954a5275e13d5a3c8438a08
Xenophage02 <79502692+Xenophage02@users.noreply.github.com>
2021-04-22 10:15:08 +0900

    Create PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    // Start is called before the first frame update
+    // 속도 조정 변수
+
+    [SerializeField]
+    private float runSpeed;
+    private float applySpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
+    // 상대 변수
+    private bool isRun = false;
+    private bool isGround = true;
+
+    private CapsuleCollider capsuleCollider;
+
+    //땅 착지 여부
+
+
+    //필요한 컴포넌트
+    [SerializeField]
+    private Rigidbody myRigid;
+    [SerializeField]
+    private Camera theCamera;
+
+
+
+
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private float lookSensitivity;
+
+    //카메라 한계
+
+    [SerializeField]
+    private float cameraRotationLimit;
+    private float currentCameraRotationX = 0;
+    
+    
+    void Start() {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        myRigid = GetComponent<Rigidbody>();
+        applySpeed = walkSpeed;
+
+        }
+
+    // Update is called once per frame
+    void Update(){
+
+        IsGround();
+        TryJump();
+        TryRun();
+        Move();
+        CameraRotation();
+        CharacterRotation();
+        
+    }
+
+    private void TryJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) && isGround
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        myRigid.velocity = transform.up * jumpForce;
+    }
+
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y);
+    }
+
+    private void TryRun()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    private void Running() //달리기
+    {
+
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
+
+    private void Move()
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _moveDirX;
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
+
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+        
+
+    }
+
+    private void CharacterRotation()
+    {
+        //좌우 회전
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
+        Debug.Log(myRigid.rotation);
+        Debug.Log(myRigid.rotation.eulerAngles);
+    }
+    
+    private void CameraRotation()
+    {
+        // 상하 회전
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotationX = _xRotation * lookSensitivity;
+        currentCameraRotationX -= _cameraRotationX; ///_cameraRotationX; 원래 이거 있었음
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+}
